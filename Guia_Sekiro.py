eccbf73312e7d9d7a6e45148dacb78b3d5e1a1c7
Alskito <zamudioalexander2@gmail.com>
2025-08-22 20:15:13 -0600

    Add files via upload

---
+++ Guia_Sekiro.py
@@ -0,0 +1,53 @@
+Algoritmo para conseguir todos los logros de sekiro
+
+#variables que voy a usar(probablemente añada mas en el futuro)#
+
+logros_completados: (Que logros ya se consiguieron)
+logros_faltantes: (Que logros faltan por conseguir)
+finales_completados: (Que finales ya se complearon, en total son 4)
+Objetivo_actual: (Que final se quiere conseguir)
+
+
+#Ahora si el algoritmo#
+
+1. Preguntar al usuario si es su primera partida
+-Si si
+    2. Completar el juego 
+    3. Matar a todos lo jefes principales
+    4. Conseguir algun final
+    5. Agregar en la variable que logros ya fueron logros_completados
+    6. Agregar en la variable que final fue conseguido
+
+-Si no
+    2. preguntar al usuario que finales ya completo 
+        Agregar en la variable que final fue conseguido
+    3. Ver que finales faltan por hacer
+        - Final Shura
+        - Final Purificación
+        - Final Retorno
+        - Final Inmortal Severed 
+    4. Preguntar al usuario que final quiere hacer ahora
+        Guardar su respuesta en la variable
+
+5. Generar la lista de logros faltantes comparando con los logros que ya se consiguieron
+6. Clasificar los logros en categorias
+    -Derrota de jefes principales y opcionales
+   - Obtención de herramientas prostéticas
+   - Mejoras de herramientas prostéticas
+   - Habilidades desbloqueadas (árbol de habilidades)
+   - Recolección de objetos clave (semillas de calabaza, cuentas de oración, etc.)
+   - Logros relacionados con los diferentes finales
+
+7. De acuerdo a los logros faltantes se sugiriria la ruta mas eficiente
+#Personalmente, yo ya complete el juego al 100%, por lo que se cuales son las maneras mas eficientes de conseguir los logros >:D#
+    -Que permita obtener el final que eligieron
+    -Evitar repetir logros que ya se consiguieron
+    -Recomendar la ruta que permita conseguir la mayor cantidad de logros 
+
+8. Terminar la partida
+9. Actualizar la variable de logros completados 
+10. Repetir los pasos hasta que la variable de logros faltantes este completa 
+11. Felicitar al usuario por todo el sufrimiento por el que tuvo que pasar
+
+#Fin del algoritmo#
+#Hasta ahora se ve muy sensillo, pero estoy seguro de que voy a sufrir codificando esto#
