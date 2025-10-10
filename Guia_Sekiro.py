2b9165810ac4c6735564dc281acdc9f93dfb28ff
Alskito <zamudioalexander2@gmail.com>
2025-10-10 16:26:13 -0600

    Avance 7 Guia_Sekiro.py

--- Guia_Sekiro.py
+++ Guia_Sekiro.py
@@ -59,7 +59,7 @@ Objetivo_actual: (Que final se quiere conseguir)
 import os
 
 # Lista de los logros totales
-logros_totales = [
+logros_sin_finales = [
     "Man Without Equal", "Ashina Traveler", "Master of the Prosthetic", "Height of Technique", "All Prosthetic Tools",
     "All Ninjutsu Techniques", "Peak Physical Strength", "Ultimate Healing Gourd", "Sword Saint, Isshin Ashina",  "Master of the Arts", 
     "Lazuline Upgrade", "Revered Blade", "Shinobi Prosthetic", "Memorial Mob",  "Resurrection", 
@@ -70,6 +70,9 @@ logros_totales = [
 
 # Chiqui lista de los finales
 finales = ["Shura", "Purification", "Return", "Immortal Severed"]
+
+#Lista anidada para las 2 listas de logros
+logros_totales = [logros_sin_finales, finales]
 
 # Pregunta que finales ya se consiguieron y se añaden como logros a un lista nueva llama logros_conseguidos
 def pedir_finales(logros_conseguidos):
@@ -86,20 +89,20 @@ def pedir_finales(logros_conseguidos):
 
 # Condicionales para que si ya consiguio un final, se añadan automaticamente los logros que se obtendrian por default 
     if any(final in logros_conseguidos for final in finales):
-        logros_conseguidos.extend([logros_totales[8],logros_totales[11], logros_totales[12], logros_totales[15],logros_totales[17], logros_totales[18], 
-        logros_totales[20]])
+        logros_conseguidos.extend([logros_sin_finales[8],logros_sin_finales[11], logros_sin_finales[12], logros_sin_finales[15],logros_sin_finales[17], logros_sin_finales[18], 
+        logros_sin_finales[20]])
 
     if finales[1] in logros_conseguidos or finales[2] in logros_conseguidos or finales[3] in logros_conseguidos:
-        logros_conseguidos.extend([logros_totales[21], logros_totales[23],logros_totales[24]])
+        logros_conseguidos.extend([logros_sin_finales[21], logros_sin_finales[23],logros_sin_finales[24]])
 
     if "Shura" in logros_conseguidos:
-        logros_conseguidos.extend([logros_totales[25]])
+        logros_conseguidos.extend([logros_sin_finales[25]])
         
     if "Purification" in logros_conseguidos:
-        logros_conseguidos.extend([logros_totales[22]])
+        logros_conseguidos.extend([logros_sin_finales[22]])
 
     if  "Return" in logros_conseguidos:
-        logros_conseguidos.extend([logros_totales[27]]) 
+        logros_conseguidos.extend([logros_sin_finales[27]]) 
 
     os.system("cls")
     return logros_conseguidos
@@ -108,7 +111,7 @@ def pedir_finales(logros_conseguidos):
 def pedir_logros(logros_conseguidos):
     print("Ahora vamos a registrar los logros.")
          
-    for logro in [logros for logros in logros_totales if logros not in logros_conseguidos]:
+    for logro in [logros for logros in logros_sin_finales if logros not in logros_conseguidos]:
         respuesta = input("¿Has conseguido " + logro + "? (s/n): ").lower()
         os.system("cls")
         while respuesta not in ["s", "n"]:
@@ -123,18 +126,20 @@ def pedir_logros(logros_conseguidos):
 
 # Muestra cuantos logros ya completaste, cuales te faltan y el porcentaje del progreso
 def mostrar_progreso(logros_conseguidos):
-    logros_faltantes =  [logros for logros in logros_totales + finales if logros not in logros_conseguidos]
+    #Nueva variable que junta ambas listas de logros
+    todos_los_logros = logros_totales[0] + logros_totales[1]
+    logros_faltantes = [logro for logro in todos_los_logros if logro not in logros_conseguidos]
     
-    print("\nHas conseguido ", len(logros_conseguidos)," de ",len(logros_totales) + len(finales)," logros.")
-    
-    if len(logros_totales) + len(finales) - len(logros_conseguidos) != 0:
+    print("\nHas conseguido", len(logros_conseguidos), "de", len(todos_los_logros), "logros.")
+
+    if len(todos_los_logros) - len(logros_conseguidos) != 0:
         print("\nTe faltan los siguientes logros:")
         for logros in logros_faltantes:
             print("-", logros)
     else:
         print("\n¡Felicidades! Completaste el juego al 100%")
 
-    porcentaje = (len(logros_conseguidos) / (len(logros_totales) + len(finales))) * 100
+    porcentaje = (len(logros_conseguidos) / len(todos_los_logros)) * 100
     print("\nPorcentaje de logros completados: %.2f" % porcentaje)
 
 # llama a las funciones
@@ -144,6 +149,8 @@ logros_conseguidos = pedir_finales(logros_conseguidos)
 logros_conseguidos = pedir_logros(logros_conseguidos)
 mostrar_progreso(logros_conseguidos)
 
+#Realmente no le veo mucho sentido a poner una lista anidada, pero pus si
 
-# Hacer este avance tomo demasiadas horas, odio las listas, probablemente matrices me terminen de matar
+
+
 
